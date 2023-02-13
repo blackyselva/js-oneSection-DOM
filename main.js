3ea92d0a3cff5c7e54d1d6b29b87b513b2962339
blackyselva <selvam17196@gmail.com>
2023-02-13 23:08:18 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,32 @@
+let section = document.createElement("section");
+document.body.appendChild(section);
+
+let container = document.createElement("container");
+section.appendChild(container);
+
+let row = document.createElement("row");
+container.appendChild(row);
+
+let col1 = document.createElement("col1");
+row.appendChild(col1);
+
+let h2 = document.createElement("h2");
+h2.innerText = "Blockchain provide you best services"
+col1.appendChild(h2);
+
+let p = document.createElement("p");
+p.innerText = "Our Blockchain opt in to the projects they genuinely want to work on.maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then Our consultants opt in to the projects they genuinely want to work on. maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then business objectives and then Our consultants."
+col1.appendChild(p);
+
+let col2 = document.createElement("col2");
+row.appendChild(col2);
+
+let h3 = document.createElement("h2");
+h3.innerText = "You can swap Graphbit token anytime"
+col2.appendChild(h3);
+
+let pp = document.createElement("p");
+pp.innerText = "Blockchain opt in to the projects they genuinely want to work on.maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then Our consultants opt in to the projects they genuinely want to work on."
+col2.appendChild(pp);
+
+console.log(section);
